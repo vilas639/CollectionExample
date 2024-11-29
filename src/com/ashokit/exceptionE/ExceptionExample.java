e7de86b6f29b62ca17b2dfb42b3c200e9e231ef5
vilas639 <vilasjdhv639@gmail.com>
2024-11-29 12:30:21 +0530

    Exceptiom prepare

--- src/com/ashokit/exceptionE/ExceptionExample.java
+++ src/com/ashokit/exceptionE/ExceptionExample.java
@@ -5,6 +5,28 @@ public class ExceptionExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		/*
+		Exception is an unwanted or unexpected event, which occurs during the execution of a
+         program
+         
+         Error is irrecoverible condition such jvm running out of memeory , memory leak, 
+         libray incapabilty
+         
+         Throwable
+         Exception  
+         compile time checked exception file not found, sql exception 
+         runtime tim uncheck exception  aioutbox , nullpointer exception
+         Error
+          viretual memoory
+          assert error
+          
+          try
+           catch 
+           finalyy 
+           throw  - throw exception explicity
+            thorws - thows the method level with multple exception 
+		 */
 	}
 
 }
