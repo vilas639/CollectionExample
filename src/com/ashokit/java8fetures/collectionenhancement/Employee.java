6142d05e4eb144a68d4e4050bc3c8ef4e6320652
vilas Jadhav <vilasjdhv639@gmail.com>
2025-01-03 23:57:02 +0530

    Update Employee.java

--- src/com/ashokit/java8fetures/collectionenhancement/Employee.java
+++ src/com/ashokit/java8fetures/collectionenhancement/Employee.java
@@ -1,19 +1,28 @@
 package com.ashokit.java8fetures.collectionenhancement;
 
+import java.time.LocalDate;
+
 public class Employee {
 	
 	private int empid;
 	
 	private String emp_name;
 	
-	private String salary;
+	
+	
+	private double salary;
+	
+	private String city;
+
+	private LocalDate dob;
+
 
 	public Employee() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Employee(int empid, String emp_name, String salary) {
+	public Employee(int empid, String emp_name, Double salary) {
 		super();
 		this.empid = empid;
 		this.emp_name = emp_name;
@@ -36,11 +45,35 @@ public class Employee {
 		this.emp_name = emp_name;
 	}
 
-	public String getSalary() {
+	public Double getSalary() {
 		return salary;
 	}
 
-	public void setSalary(String salary) {
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	
+	
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+	public void setSalary(double salary) {
 		this.salary = salary;
 	}
 
