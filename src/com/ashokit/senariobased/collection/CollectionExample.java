de5b464bf8c7cc38ab6f46e97c4c2a947cce25b2
vilas Jadhav <vilasjdhv639@gmail.com>
2025-01-13 11:47:35 +0530

    Update CollectionExample.java

--- src/com/ashokit/senariobased/collection/CollectionExample.java
+++ src/com/ashokit/senariobased/collection/CollectionExample.java
@@ -1,7 +1,11 @@
 package com.ashokit.senariobased.collection;
+package com.example.collection;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CollectionExample {
 
@@ -12,10 +16,11 @@ public class CollectionExample {
 Answer:
 Use Collections.unmodifiableList or List.of (Java 9+):
       */
-        List<String> immutableList = List.of("A", "B", "C");
+    
+      //  List<String> immutableList = List.of("A", "B", "C");
 
        // immutableList.add("D");
-        System.out.println(immutableList.toString());
+        //System.out.println(immutableList.toString());
 
 
 
@@ -87,8 +92,149 @@ Use Collections.unmodifiableList or List.of (Java 9+):
                 .sorted(Comparator.comparing(Person::getAge))
         .forEach(p -> System.out.println (p));
 
+        
+        //merge 2 maps
+        //Map<String, Integer> map1 = Map.of("a", 1, "b", 2);
+        //Map<String, Integer> map2 = Map.of("b", 3, "c", 4);
+        
+        Map<String,Integer> map1= new HashMap<>();
+        map1.put("a",1);
+        map1.put("b",2);
+        
+        Map<String,Integer> map2= new HashMap<>();
+        map2.put("b",3);
+        map2.put("c",4);
+
+        Map<String, Integer> mergedMap = Stream.of(map1, map2)
+                .flatMap(map -> map.entrySet().stream())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (value1, value2) -> value1 + value2 // merge values
+                ));
+
+        System.out.println(mergedMap); // {a=1, b=5, c=4}
 
 
+
+        
+        CopyOnWriteArrayList<String> list2 = new CopyOnWriteArrayList<>(Arrays.asList("A", "B", "C"));
+        for (String s : list2) {
+            list2.add("D");
+        }
+        
+        System.out.println(list2.toString());
+        
+//        9. How would you find the first non-repeated character in a string using collections?
+//        		Answer:
+//        		Use a LinkedHashMap:
+
+        		
+        		String str = "swiss";
+        		Map<Character, Long> map = str.chars().mapToObj(s -> (char) s)
+        				.collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+        		
+        		char firstNonRepeated = map.entrySet().stream().
+        				filter(e -> e.getValue() == 1)
+        				.map(Map.Entry::getKey)
+        				.findFirst()
+        				.orElse(null);
+
+        		System.out.println(firstNonRepeated);
+        		
+        		
+        		
+        		
+//        		11. How would you find duplicates in a list?
+//        				Answer:
+//        				Use a Set to track seen elements:
+
+        		        List<Integer> list4 = new ArrayList<>();
+        		        list4.add(4);
+        		        list4.add(2);
+        		        list4.add(5);
+        		        list4.add(7);
+        		        list4.add(2);
+        		       
+        			
+        				Set<Integer> duplicates = list4.stream().
+        						distinct()
+        						.collect(Collectors.toSet());
+        				
+        				System.out.println(duplicates.toString());
+        				
+        				
+//        				How would you reverse a list?
+//        						Answer:
+//        						Use Collections.reverse:
+
+        						List<Integer> list6 = Arrays.asList(1, 2, 3);
+        						Collections.reverse(list6);
+
+                				System.out.println(list6.toString());
+
+//                				
+//                				13. How would you sort a map by its values?
+//                						Answer:
+//                						Use a Comparator:
+
+                						Map<String, Integer> map3 = new HashMap<>();
+                						map3.put("A",5);
+                						map3.put("V",2);
+                						map3.put("C",1);
+                						map3.put("P",9);
+                						
+                						map3.entrySet().stream()
+                						   .sorted(Map.Entry.comparingByValue())  // c,v,a,p
+                						   .forEachOrdered(System.out::println);
+  
+//                						15. How would you handle concurrent modifications during iteration?
+//                								Answer:
+//                								Use ConcurrentHashMap:
+
+                								ConcurrentHashMap<String, String> map4 = new ConcurrentHashMap<>();
+                								map4.put("A", "1");
+
+                								for (String key : map4.keySet()) {
+                								    map4.put("B", "2");
+                								}
+                								
+                								System.out.println(map4.toString());
+                					
+//                								17. How would you remove all elements from a collection that match a condition?
+//                										Answer:
+//                										Use removeIf:
+                								 List<Integer> list5 = new ArrayList<>();
+                			        		        list5.add(4);
+                			        		        list5.add(2);
+                			        		        list5.add(5);
+                			        		        list5.add(17);
+                			        		        list5.add(2);
+                			        		       
+                										list5.removeIf(n -> n < 10);	 //17
+                										System.out.println(list5.toString());
+                										
+                										
+//                										18. How would you merge two lists without duplicates?
+//                												Answer:
+//                												Use a Set:
+                										List<String> list7 = Arrays.asList("a", "b", "c");
+                										List<String> list8 = Arrays.asList("b", "c", "d");
+
+                										List<String> mergedList = Stream.concat(list7.stream(), list8.stream())
+                										        .distinct()
+                										        .collect(Collectors.toList());
+
+                										System.out.println(mergedList); // [a, b, c, d]
+                										
+                										PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+                										pq.add(10);
+                										pq.add(5);
+                										pq.add(12);
+                										pq.add(3);
+                										System.out.println(pq.poll());//12   //remove the elemet
+                										System.out.println(pq); //10 ,5, 3
+    
     }
 }
 
