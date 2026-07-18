bed387e1a183ac94308ff96e55ec2ccb4d4d8884
vilas639 <vilasjdhv639@gmail.com>
2026-07-18 16:45:16 +0530

    array with range

--- src/codingblocks/arrays/ReverseArrayDemo.java
+++ src/codingblocks/arrays/ReverseArrayDemo.java
@@ -5,9 +5,10 @@ public class ReverseArrayDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] arr= {1,2,3,4,5,6};
-		//reverseArray(arr);
-		reverseArrayInPlace(arr);
+		int[] arr= {1,2,3,4,5,6,8,9,10};
+		
+		//reverseArrayInPlace(arr);
+		reverseArraywithRange(arr, 2, 6);
 		for(int i=0;i<arr.length;i++)
 		{
 			System.out.print(arr[i]+" ");
@@ -27,6 +28,23 @@ public class ReverseArrayDemo {
 		arr[j]=temp;
 		i++;
 		j--;
+		}
+		
+	}
+	
+	private static void reverseArraywithRange(int[] arr,int start,int end)
+	{
+		if(start>=end)
+		{
+			return;
+		}
+		while(start<end)
+		{
+		int temp=arr[start];
+		arr[start]=arr[end];
+		arr[end]=temp;
+		start++;
+		end--;
 		}
 		
 	}
