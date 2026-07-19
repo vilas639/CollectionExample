d7bceef9b58c2b0698a8d5cebbe8a66dd4c82247
vilas639 <vilasjdhv639@gmail.com>
2026-07-20 00:42:51 +0530

    2D Array

---
+++ src/codingblocks/Sorting/BubbleSortDemo.java
@@ -0,0 +1,28 @@
+package codingblocks.Sorting;
+
+public class BubbleSortDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr = { 2, 13, -4, 5, 7 };
+		Sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+	private static void Sort(int[] arr) {
+		// TODO Auto-generated method stub
+		for (int turn = 1; turn < arr.length; turn++) {
+			for (int i = 0; i < arr.length - turn; i++) {
+				if (arr[i] > arr[i + 1]) {
+					int temp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
+				}
+			}
+		}
+	}
+
+}
---
+++ src/codingblocks/Sorting/Selection_Sort.java
@@ -0,0 +1,33 @@
+package codingblocks.Sorting;
+
+public class Selection_Sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr = { 2, 13, -4, 5, 7 };
+		Sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+	
+	public static void Sort(int[] arr) {
+
+		for (int i = 0; i < arr.length; i++) {
+			int mini = i;// index
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j] < arr[mini]) {
+					mini = j;
+				}
+
+			}
+			//swap logic
+			int temp = arr[i];
+			arr[i] = arr[mini];
+			arr[mini] = temp;
+		}
+
+	}
+
+}
