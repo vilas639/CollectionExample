a78249a55ba28ad0734fbda3de4b6760112dee43
vilas639 <vilasjdhv639@gmail.com>
2026-07-20 17:57:22 +0530

    String

---
+++ src/codingblocks/String/String_Dem_3.java
@@ -0,0 +1,14 @@
+package codingblocks.String;
+
+public class String_Dem_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s = "hello";
+		System.out.println(10 + s + (30 + 40));
+		System.out.println(10 + s + 30 + 40);
+		System.out.println(10 + 30 + 40+s);
+
+	}
+}
---
+++ src/codingblocks/String/String_Demo_2.java
@@ -0,0 +1,16 @@
+package codingblocks.String;
+
+public class String_Demo_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "hey";
+		String s2 = "Bye";
+		s1 = s1 + s2;
+		String str1 = new String("hello");
+		String str2 = new String("bye");
+		str1 = str1 + str2;
+		s2 = s2 + "okey";
+		String s = "hey" + "bye";
+	}
+}
