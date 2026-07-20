b170dd98d4e8d1b7d71dca29b7d166fd172a7848
vilas639 <vilasjdhv639@gmail.com>
2026-07-20 21:44:55 +0530

    Arralist two number

---
+++ src/codingblocks/Arraylist/ArrayList_Working.java
@@ -0,0 +1,17 @@
+package codingblocks.Arraylist;
+
+import java.util.ArrayList;
+
+public class ArrayList_Working {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> ll = new ArrayList<>(90);
+		ll.add(10);
+		ll.add(20);
+		ll.add(30);
+		ll.add(40);
+		ll.add(50);
+		ll.add(2, -6);
+	}
+}
