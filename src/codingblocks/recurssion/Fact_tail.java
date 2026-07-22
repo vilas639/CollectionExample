7cf4e22f8e8e2874c8661f67249f9e43e75b4e03
vilas639 <vilasjdhv639@gmail.com>
2026-07-22 14:14:24 +0530

    Recurssion

---
+++ src/codingblocks/recurssion/Fact_tail.java
@@ -0,0 +1,20 @@
+package codingblocks.recurssion;
+
+public class Fact_tail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		System.out.println(fact(n, 1));
+
+	}
+
+	public static int fact(int n, int ans) {
+		if (n == 0) {
+			
+		}
+
+		return fact(n - 1, ans * n);// sp
+
+	}
+}
---
+++ src/codingblocks/recurssion/SubSequnce.java
@@ -0,0 +1,25 @@
+package codingblocks.recurssion;
+
+public class SubSequnce {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String ques = "abc";
+		String ans = "";
+		PrintSubSeqnue(ques, ans);
+
+	}
+
+	public static void PrintSubSeqnue(String ques, String ans) {
+		// TODO Auto-generated method stub
+		if (ques.length() == 0) {
+			System.out.print(ans + " ");
+			return;
+		}
+
+		char ch = ques.charAt(0);
+		PrintSubSeqnue(ques.substring(1), ans);
+		PrintSubSeqnue(ques.substring(1), ans + ch);
+
+	}
+}
