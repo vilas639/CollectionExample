5ef69055e5c9f51a09195a79416ee669a0d6dbce
vilas639 <vilasjdhv639@gmail.com>
2026-07-25 15:50:39 +0530

    Binary Tree

---
+++ src/codingblocks/binarytree/BinaryTree.java
@@ -0,0 +1,182 @@
+package codingblocks.binarytree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BinaryTree {
+
+	public class Node {
+		int val;
+		Node left;
+		Node right;
+
+	}
+
+	Scanner sc = new Scanner(System.in);
+	private Node root;
+
+	public BinaryTree() {
+		// TODO Auto-generated constructor stub
+		root = CreateTree();
+	}
+
+	private Node CreateTree() {
+		// TODO Auto-generated method stub
+
+		int item = sc.nextInt();
+		Node nn = new Node();
+		nn.val = item;
+		boolean hlc = sc.nextBoolean();
+		if (hlc) {
+			nn.left = CreateTree();
+
+		}
+		boolean hrc = sc.nextBoolean();
+		if (hrc) {
+			nn.right = CreateTree();
+		}
+		return nn;
+
+	}
+
+	public void Display() {
+		Display(root);
+	}
+
+	private void Display(Node node) {
+		if (node == null) {
+			return;
+		}
+		String s = "";
+		s = "<--" + node.val + "-->";
+		if (node.left != null) {
+			s = node.left.val + s;
+
+		} else {
+			s = "." + s;
+		}
+		if (node.right != null) {
+			s = s + node.right.val;
+
+		} else {
+			s = s + ".";
+		}
+		System.out.println(s);
+		Display(node.left);
+		Display(node.right);
+
+	}
+
+	public boolean find(int item) {
+
+		return find(this.root, item);
+	}
+
+	private boolean find(Node nn, int item) {
+		if (nn == null) {
+			return false;
+		}
+		if (nn.val == item) {
+			return true;
+		}
+		boolean left = find(nn.left, item);
+		boolean right = find(nn.right, item);
+		return left || right;
+
+	}
+
+	public int max() {
+
+		return max(this.root);
+	}
+
+	private int max(Node node) {
+		if (node == null) {
+			return Integer.MIN_VALUE;
+		}
+
+		int left = max(node.left);
+		int right = max(node.right);
+		return Math.max(node.val, Math.max(left, right));
+
+	}
+
+	public int ht() {
+
+		return ht(this.root);
+	}
+
+	private int ht(Node node) {
+		if (node == null) {
+			return -1;
+		}
+
+		int left = ht(node.left);
+		int right = ht(node.right);
+		return Math.max(left, right) + 1;
+
+	}
+
+	public void PreOrder() {
+		PreOrder(this.root);
+		System.out.println();
+	}
+
+	private void PreOrder(Node node) {
+		if (node == null) {
+			return;
+		}
+		System.out.print(node.val + " ");
+		PreOrder(node.left);
+		PreOrder(node.right);
+
+	}
+
+	public void POStOrder() {
+		POStOrder(this.root);
+		System.out.println();
+	}
+
+	private void POStOrder(Node node) {
+		if (node == null) {
+			return;
+		}
+		POStOrder(node.left);
+		POStOrder(node.right);
+		System.out.print(node.val + " ");
+
+	}
+
+	public void INOrder() {
+		INOrder(this.root);
+		System.out.println();
+	}
+
+	private void INOrder(Node node) {
+		// TODO Auto-generated method stub
+		if (node == null) {
+			return;
+		}
+		INOrder(node.left);
+		System.out.print(node.val + " ");
+		INOrder(node.right);
+
+	}
+
+	public void levelOrder() {
+		Queue<Node> q = new LinkedList<>();
+		q.add(root);
+		while (!q.isEmpty()) {
+			Node rv = q.poll();// remove first
+			System.out.print(rv.val + " ");
+			if (rv.left != null) {
+				q.add(rv.left);
+			}
+			if (rv.right != null) {
+				q.add(rv.right);
+			}
+		}
+		System.out.println();
+	}
+}
--- src/codingblocks/linklist/LinkedList_java.java
+++ src/codingblocks/linklist/LinkedList_java.java
@@ -1,5 +1,21 @@
 package codingblocks.linklist;
-
+import java.util.LinkedList;
 public class LinkedList_java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<Integer> ll = new LinkedList<>();
+		ll.add(12);
+		ll.add(4);
+		ll.add(34);
+		ll.add(56);
+		ll.add(0, 6);
+		System.out.println(ll);
+		System.out.println(ll.size());
+		for (int i = 0; i < ll.size(); i++) {
+			System.out.println(ll.get(i));
+		}
+
+	}
 
 }
