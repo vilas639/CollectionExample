a3f1f12b8166b2ae2d85e45c6b585e8e2b7196b9
vilas639 <vilasjdhv639@gmail.com>
2026-07-27 22:21:47 +0530

    heap

---
+++ src/codingblocks/heap/Java_PriorityQueue.java
@@ -0,0 +1,14 @@
+package codingblocks.heap;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Java_PriorityQueue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PriorityQueue<Integer> pq = new PriorityQueue<>();// min heap
+		PriorityQueue<Integer> pq1 = new PriorityQueue<>(Collections.reverseOrder());// max heap
+
+	}
+}
